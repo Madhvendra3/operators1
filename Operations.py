67f259b4cc865be41c21e27e1af3b58ee6fa99a5
Madhvendra3 <rajawatbunty@gmail.com>
2026-02-08 18:37:05 +0000

    Add files via upload

---
+++ Operations.py
@@ -0,0 +1,15 @@
+a = 1
+b = 1
+c = 1
+if a and b and c :
+    print("The boolean value is true.")
+else:
+    print("The boolean value is false.")    
+
+d = 10
+e = 18
+f = 0
+if d or e or f:
+    print("The boolean value is true.")
+else:
+    print("The boolean value is false.")
